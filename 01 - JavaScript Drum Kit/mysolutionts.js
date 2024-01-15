231623a9a6666a72b4c89abdac104abd0f543074
Claudia Lim <climweiching@gmail.com>
2024-01-15 11:01:08 +0000

    drumkit solution

--- 01 - JavaScript Drum Kit/mysolutionts.js
+++ 01 - JavaScript Drum Kit/mysolutionts.js
@@ -1,6 +1,71 @@
-var clapButton = document.querySelector("#clap");
 var clapSound = document.querySelector("#clap-sound");
-function onClick() {
-    clapSound.play();
+var hihatSound = document.querySelector("#hihat-sound");
+var kickSound = document.querySelector("#kick-sound");
+var openhatSound = document.querySelector("#openhat-sound");
+var boomSound = document.querySelector("#boom-sound");
+var rideSound = document.querySelector("#ride-sound");
+var snareSound = document.querySelector("#snare-sound");
+var tomSound = document.querySelector("#tom-sound");
+var tinkSound = document.querySelector("#tink-sound");
+var clapKey = document.querySelector("#clap");
+var hihatKey = document.querySelector("#hihat");
+var kickKey = document.querySelector("#kick");
+var openhatKey = document.querySelector("#openhat");
+var boomKey = document.querySelector("#boom");
+var rideKey = document.querySelector("#ride");
+var snareKey = document.querySelector("#snare");
+var tomKey = document.querySelector("#tom");
+var tinkKey = document.querySelector("#tink");
+function keyTimeOut(key) {
+    key.classList.remove("key-press");
 }
-clapButton.addEventListener("click", onClick);
+window.addEventListener('keydown', function (e) {
+    console.log(e.key);
+    switch (e.key) {
+        case "a":
+            clapSound.play();
+            clapKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, clapKey);
+            break;
+        case "s":
+            hihatSound.play();
+            hihatKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, hihatKey);
+            break;
+        case "d":
+            kickSound.play();
+            kickKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, kickKey);
+            break;
+        case "f":
+            openhatSound.play();
+            openhatKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, openhatKey);
+            break;
+        case "g":
+            boomSound.play();
+            boomKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, boomKey);
+            break;
+        case "h":
+            rideSound.play();
+            rideKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, rideKey);
+            break;
+        case "j":
+            snareSound.play();
+            snareKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, snareKey);
+            break;
+        case "k":
+            tomSound.play();
+            tomKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, tomKey);
+            break;
+        case "l":
+            tinkSound.play();
+            tinkKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, tinkKey);
+            break;
+    }
+});
--- 01 - JavaScript Drum Kit/mysolutionts.ts
+++ 01 - JavaScript Drum Kit/mysolutionts.ts
@@ -1,7 +1,74 @@
-const clapButton = document.querySelector<HTMLDivElement>("#clap");
 const clapSound = document.querySelector<HTMLAudioElement>("#clap-sound")
-function onClick(): void {
-    clapSound.play();
-}
+const hihatSound = document.querySelector<HTMLAudioElement>("#hihat-sound");
+const kickSound = document.querySelector<HTMLAudioElement>("#kick-sound");
+const openhatSound = document.querySelector<HTMLAudioElement>("#openhat-sound");
+const boomSound = document.querySelector<HTMLAudioElement>("#boom-sound");
+const rideSound = document.querySelector<HTMLAudioElement>("#ride-sound");
+const snareSound = document.querySelector<HTMLAudioElement>("#snare-sound");
+const tomSound = document.querySelector<HTMLAudioElement>("#tom-sound");
+const tinkSound = document.querySelector<HTMLAudioElement>("#tink-sound");
 
-clapButton.addEventListener("click", onClick);
+const clapKey = document.querySelector<HTMLDivElement>("#clap");
+const hihatKey = document.querySelector<HTMLDivElement>("#hihat");
+const kickKey = document.querySelector<HTMLDivElement>("#kick");
+const openhatKey = document.querySelector<HTMLDivElement>("#openhat");
+const boomKey = document.querySelector<HTMLDivElement>("#boom");
+const rideKey = document.querySelector<HTMLDivElement>("#ride");
+const snareKey = document.querySelector<HTMLDivElement>("#snare");
+const tomKey = document.querySelector<HTMLDivElement>("#tom");
+const tinkKey = document.querySelector<HTMLDivElement>("#tink");
+
+function keyTimeOut (key: HTMLDivElement): void {
+    key.classList.remove("key-press");
+}
+window.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    switch(e.key) {
+        case "a" :
+            clapSound.play();
+            clapKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, clapKey);
+            break;
+        case "s" :
+            hihatSound.play();
+            hihatKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, hihatKey);
+            break;
+        case "d" :
+            kickSound.play();
+            kickKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, kickKey);
+            break;
+        case "f" :
+            openhatSound.play();
+            openhatKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, openhatKey);
+            break;
+        case "g" :
+            boomSound.play();
+            boomKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, boomKey);
+            break;
+        case "h" :
+            rideSound.play();
+            rideKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, rideKey);
+            break;
+        case "j" :
+            snareSound.play();
+            snareKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, snareKey);
+            break;
+        case "k" :
+            tomSound.play();
+            tomKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, tomKey);
+            break;
+        case "l" :
+            tinkSound.play();
+            tinkKey.classList.add("key-press");
+            setTimeout(keyTimeOut, 500, tinkKey);
+            break;
+        }
+})
+
