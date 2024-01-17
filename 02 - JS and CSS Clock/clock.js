9a1e77566d98a200b0416787013f63e9c97e20e7
Claudia Lim <climweiching@gmail.com>
2024-01-17 13:15:43 +0000

    ts clock

---
+++ 02 - JS and CSS Clock/clock.js
@@ -0,0 +1,26 @@
+var secondsHand = document.querySelector("#seconds-hand");
+var minutesHand = document.querySelector("#minutes-hand");
+var hoursHand = document.querySelector("#hours-hand");
+var clockFace = document.querySelector("#clock-face");
+for (var i = 0; i < 360; i += 30) {
+    clockFace.innerHTML += "<div class=\"clock-mark\" style=\"transform: rotate(".concat(i, "deg);\"><div class=\"mark\"></div></div>");
+}
+function showTime() {
+    var currentTime = new Date();
+    var currentSeconds = currentTime.getSeconds();
+    var secondsDegrees = ((currentSeconds / 60) * 360) + 90;
+    if (secondsDegrees == 90) {
+        secondsHand.style.transition = 'all 0s';
+    }
+    else {
+        secondsHand.style.transition = 'all 0.5s cubic-bezier(0.43, 0.45, 0, 1.64)';
+    }
+    secondsHand.style.transform = "rotate(".concat(secondsDegrees, "deg)");
+    var currentMinutes = currentTime.getMinutes();
+    var minutesDegrees = ((currentMinutes / 60) * 360) + 90;
+    minutesHand.style.transform = "rotate(".concat(minutesDegrees, "deg)");
+    var currentHour = currentTime.getHours();
+    var hourDegrees = ((currentHour / 12) * 360) + 90;
+    hoursHand.style.transform = "rotate(".concat(hourDegrees, "deg)");
+}
+setInterval(showTime, 1000);
---
+++ 02 - JS and CSS Clock/clock.ts
@@ -0,0 +1,29 @@
+const secondsHand = document.querySelector<HTMLDivElement>("#seconds-hand");
+const minutesHand = document.querySelector<HTMLDivElement>("#minutes-hand");
+const hoursHand = document.querySelector<HTMLDivElement>("#hours-hand");
+const clockFace = document.querySelector<HTMLDivElement>("#clock-face");
+
+for (let i: number = 0; i < 360; i+=30) {
+    clockFace.innerHTML += `<div class="clock-mark" style="transform: rotate(${i}deg);"><div class="mark"></div></div>`
+}
+function showTime (): void {
+    const currentTime: Date = new Date();
+    const currentSeconds: number = currentTime.getSeconds();
+    let secondsDegrees: number = ((currentSeconds / 60) * 360) + 90;
+    if (secondsDegrees == 90) {
+        secondsHand.style.transition = 'all 0s';
+    } else {
+        secondsHand.style.transition = 'all 0.5s cubic-bezier(0.43, 0.45, 0, 1.64)';
+    }
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const currentMinutes: number = currentTime.getMinutes();
+    const minutesDegrees: number = ((currentMinutes/60) * 360) + 90;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const currentHour: number = currentTime.getHours();
+    const hourDegrees: number = ((currentHour/12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(showTime, 1000);
